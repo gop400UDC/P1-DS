c1190354cd20cfaffba3cbe447ae10a438ffa0c3
gop400UDC <g.oliveira@udc.es>
2024-10-11 23:54:36 +0200

    Update StringGames.java

--- p1Ds/src/e1/StringGames.java
+++ p1Ds/src/e1/StringGames.java
@@ -1,98 +1,103 @@
 package e1;
 
-import java.util.HashSet;
-
-import java.util.Set;
 
 public class StringGames {
+    public static String bestCharacters(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            throw new IllegalArgumentException("Las dos cadenas deben tener la misma longitud");
+        }
+        int minusculasS1=0;
+        int minusculasS2=0;
+        int mayusculasS1=0;
+        int mayusculasS2=0;
+        int digitosS1=0;
+        int digitosS2=0;
 
-    // Método 1: Determina cuál String gana en más categorías
-    public static String bestCharacters(String str1, String str2) {
-        if (str1.length() != str2.length()) {
-            throw new IllegalArgumentException("Las cadenas deben tener la misma longitud.");
+        for (int i=0;i<s1.length();i++) {
+            char c1 =s1.charAt(i);
+            char c2 =s2.charAt(i);
+
+            if (Character.isLowerCase(c1)) {
+                minusculasS1++;
+            }else if(Character.isUpperCase(c1)) {
+                mayusculasS1++;
+            }else if(Character.isDigit(c1)) {
+                digitosS1++;
+            }
+            if(Character.isLowerCase(c2)) {
+                minusculasS2++;
+            }else if(Character.isUpperCase(c2)) {
+                mayusculasS2++;
+            }else if(Character.isDigit(c2)) {
+                digitosS2++;
+            }
+        }
+        int puntosS1 =0;
+        int puntosS2 =0;
+        if(minusculasS1 > minusculasS2) {
+            puntosS1++;
+        }else if(minusculasS2 > minusculasS1) {
+            puntosS2++;
         }
 
-        int[] scores = new int[3]; // 0: minúsculas, 1: mayúsculas, 2: dígitos
-
-        for (int i = 0; i < str1.length(); i++) {
-            if (Character.isLowerCase(str1.charAt(i))) scores[0]++;
-            if (Character.isUpperCase(str1.charAt(i))) scores[1]++;
-            if (Character.isDigit(str1.charAt(i))) scores[2]++;
-
-            if (Character.isLowerCase(str2.charAt(i))) scores[0]--;
-            if (Character.isUpperCase(str2.charAt(i))) scores[1]--;
-            if (Character.isDigit(str2.charAt(i))) scores[2]--;
+        if(mayusculasS1 > mayusculasS2) {
+            puntosS1++;
+        }else if(mayusculasS2 > mayusculasS1) {
+            puntosS2++;
+        }
+        if(digitosS1 > digitosS2) {
+            puntosS1++;
+        }else if(digitosS2 > digitosS1) {
+            puntosS2++;
         }
 
-        // Determinar el ganador basado en la cantidad de categorías ganadas
-        int str1Wins = 0;
-        int str2Wins = 0;
-
-        for (int score : scores) {
-            if (score > 0) str1Wins++;
-            else if (score < 0) str2Wins++;
-        }
-
-        if (str1Wins > str2Wins) return str1;
-        if (str2Wins > str1Wins) return str2;
-        return str2; // En caso de empate, devolver str2
+        return puntosS1 >= puntosS2 ? s1 : s2;
     }
+    public static int crossingWords(String s1, String s2) {
+        int cuenta=0;
 
-    // Método 2: Determina cuántas letras comparten dos Strings
-    public static int crossingWords(String str1, String str2) {
-        Set<Character> set1 = new HashSet<>();
-        Set<Character> set2 = new HashSet<>();
+        for (int i=0; i<s1.length();i++) {
+            char c1=s1.charAt(i);
 
-        for (char c : str1.toCharArray()) {
-            set1.add(c);
-        }
-        for (char c : str2.toCharArray()) {
-            set2.add(c);
-        }
-
-        set1.retainAll(set2); // Conserva solo los elementos que están en ambos sets
-
-        return set1.size();
-    }
-
-    // Método 3: Reordena el primer String según el segundo String
-    public static String wackyAlphabet(String str1, String str2) {
-        if (str2.length() != 26) {
-            throw new IllegalArgumentException("El segundo String debe contener todas las letras del alfabeto.");
-        }
-
-        StringBuilder sortedString = new StringBuilder();
-        Set<Character> addedChars = new HashSet<>();
-
-        // Reordenar según el segundo String
-        for (char c : str2.toCharArray()) {
-            for (char ch : str1.toCharArray()) {
-                if (ch == c && !addedChars.contains(ch)) {
-                    sortedString.append(ch);
-                    addedChars.add(ch);
+            for(int j=0;j<s2.length();j++) {
+                char c2=s2.charAt(j);
+                if (c1==c2) {
+                    cuenta++;
                 }
             }
         }
 
-        // Agregar los caracteres que no están en str2
-        for (char ch : str1.toCharArray()) {
-            if (!addedChars.contains(ch)) {
-                sortedString.append(ch);
-            }
+        return cuenta;
+    }
+
+    public static String wackyAlphabet(String s1, String s2) {
+        if(s2.length()!=26){
+            throw new IllegalArgumentException("ERROR-El segundo string no contiene todas las letras del alfabeto");
         }
 
-        return sortedString.toString();
-    }
+        for(char c='a';c<='z';c++) {
+            if (s2.indexOf(c)==-1) {
+                throw new IllegalArgumentException("ERROR-El segundo string no contiene todas las letras del alfabeto");
+            }
+        }
+        StringBuilder s3 = new StringBuilder();
+        for (int i = 0; i < s2.length(); i++) {
 
-    // Método principal para probar la clase
-    public static void main(String[] args) {
-        // Pruebas manuales (opcional)
-        System.out.println(bestCharacters("ABC123", "a12345")); // Debería imprimir "a12345"
-        System.out.println(crossingWords("abc", "bcd")); // Debería imprimir 2
-        System.out.println(wackyAlphabet("hello", "zyxwvutsrqponmlkjihgfedcba")); // Debería imprimir "ollhe"
+            char c = s2.charAt(i);
+
+            for (int j = 0; j < s1.length(); j++) {
+
+                if (s1.charAt(j) == c) {
+
+                    s3.append(c);
+
+                }
+
+            }
+
+        }
+        return s3.toString();
     }
 }
-
-
 
 
